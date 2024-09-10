6d6afb947d8e472d6d39b9b0b5d11eea81a0b597
Tutzjwlee <arthurlee@Arthurs-MacBook-Air.local>
2024-09-10 09:02:34 -0300

    Finished ex1

--- Aula18/Checksum.java
+++ Aula18/Checksum.java
@@ -1,14 +1,19 @@
-public class Checksum {
+    public class Checksum {
 
-    public void calcularChecksum(char vet[]) {
-        byte byVeto[] = new byte[vet.length]; 
-        byte total = 0; 
+        public byte[] calcularChecksum(char vet[]) {
+            byte byVeto[] = new byte[vet.length+1]; 
+            byte total = 0; 
+            int i = 0;
 
-        for (int i = 0; i < vet.length; i++) {
-            byVeto[i] = (byte) vet[i];
-            total = (byte) (total + byVeto[i]); 
+            for (i = 0; i < vet.length; i++) {
+                byVeto[i] = (byte) vet[i];
+                total = (byte) ((total + byVeto[i]) & 0xFF);
+            }
+            total = (byte) ~total;
+            total = (byte) ((total + (byte) 1) & 0xFF);
+            System.out.println("Checksum: " + total);
+            byVeto[i+1] = total;
+            return byVeto;
         }
-
-        System.out.println("Checksum: " + total);
+        
     }
-}
